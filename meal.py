7eb57d4cf7b2cc2fdf4843c42133d57503a93da1
au-059 <125726429+au-059@users.noreply.github.com>
2023-05-13 00:06:06 +0600

    Add files via upload

---
+++ meal.py
@@ -0,0 +1,18 @@
+def main():
+        time = str(input("What time is it? "))
+        new_time = convert(time)
+
+        if new_time >= 7 and new_time <=8:
+                print("Breakfast time")
+        if new_time >= 12 and new_time <=13:
+                print("Lunch time")
+        if new_time >= 18 and new_time <=19:
+                print("Dinner time")
+
+def convert(time):
+        hrs, mins = time.split(":")
+        float_mins = float(mins) / 60
+        return float(hrs) + float_mins
+
+if __name__ == "__main__":
+        main()
